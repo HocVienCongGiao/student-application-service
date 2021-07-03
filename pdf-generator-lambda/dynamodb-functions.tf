51cf926f3cf7cabfd92759518ff9dbe1b0b31b83
Peter Bean <binh@sunrise.vn>
2021-07-03 10:41:18 +0700

    First

---
+++ pdf-generator-lambda/dynamodb-functions.tf
@@ -0,0 +1,14 @@
+module "PdfGenerator" {
+  source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/dynamodb-function"
+  service_name = var.service_name
+
+  function_name = "pdf-generator"
+  table_name    = "PdfGeneration"
+  is_in_vpc     = false
+
+  environment = var.environment
+  db_host              = var.db_host
+  db_user              = var.db_user
+  db_password          = var.db_password
+  db_name              = var.db_name
+}
---
+++ pdf-generator-lambda/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    tfe = {
+      version = "~> 0.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
